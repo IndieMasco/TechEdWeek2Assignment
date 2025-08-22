5541bc26203da8e4840ba1bfbd3f4e10fa6fcb7b
sam <s.clarkyy.1994@gmail.com>
2025-08-22 21:59:52 +0100

    updated JS and image list

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const images = [
     Image:
       "https://images.unsplash.com/photo-1526481280693-3bfa7568e0f3?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     altText:
-      "Beautiful view over looking the city of Fujiyoshida with mount fuji in the background",
+      "Beautiful view over looking the city of Fujiyoshida with mount fuji in the background.",
   },
   {
     Image:
       "https://images.unsplash.com/photo-1526374073174-7661a8028eb4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    altText: "Red temple surounded by moutains, trees and a waterfall",
+    altText: "Red temple surounded by moutains, trees and a waterfall.",
   },
   {
     Image:
@@ -22,9 +22,20 @@ const images = [
   },
   {
     Image:
-      "https://images.unsplash.com/photo-1555084624-2d207284de21?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1504109586057-7a2ae83d1338?q=80&w=1333&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    altText: "floating torii gate of the Itsukushima Shrine in Japan.",
+  },
+  {
+    Image:
+      "https://images.unsplash.com/photo-1551241090-67de81d3541c?q=80&w=1209&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     altText:
-      "Jizo statues, often found in rows or groups at temples and cemeteries in Japan.",
+      "Japanese dry garden, also known as a Zen garden carefully arranged rocks and raked sand.",
+  },
+  {
+    Image:
+      "https://images.unsplash.com/photo-1480796927426-f609979314bd?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    altText:
+      "storefront of Fujiden, a Japanese restaurant specializing in Okonomiyaki and Monjayaki.",
   },
 ];
 

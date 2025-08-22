2614508da37b9a58faa8383f634bc835ed3a8652
sam <s.clarkyy.1994@gmail.com>
2025-08-22 14:18:14 +0100

    added images and alt text to my JS

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 console.log("Hello, Tech Ed Week 2!");
+
+const images = [
+  {
+    URL: "https://images.unsplash.com/photo-1526481280693-3bfa7568e0f3?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Beautiful view over looking the city of Fujiyoshida with mount fuji in the background",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1526374073174-7661a8028eb4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Red temple surounded by moutains, trees and a waterfall",
+  },
+  {
+    URL: "https://images.unsplash.com/photo-1555084624-2d207284de21?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Jizo statues, often found in rows or groups at temples and cemeteries in Japan.",
+  },
+  {
+    URL: "https://images.unsplash.com/photo-1504109586057-7a2ae83d1338?q=80&w=1333&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "floating torii gate of the Itsukushima Shrine, located on Miyajima Island",
+  },
+  {
+    URL: "https://images.unsplash.com/photo-1480796927426-f609979314bd?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Traditional japanese restaurant with bicycles outside, crates and other things out front",
+  },
+  {
+    URL: "https://images.unsplash.com/photo-1551241090-67de81d3541c?q=80&w=1209&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Japanese rock garden, also known as a Karesansui garden, with a traditional Japanese temple in the background",
+  },
+  {
+    URL: "https://images.unsplash.com/photo-1568299662505-db3686267724?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Traditional japanese clothing in three different colours white, blue, red all covered in folowers",
+  },
+  {
+    URL: "https://images.unsplash.com/photo-1602030029545-52959ef2927c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Taiyaki is made from a pancake-like batter cooked in a fish-shaped mold, typically filled with sweetened red bean paste",
+  },
+];
